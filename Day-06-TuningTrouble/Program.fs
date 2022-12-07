cf88527fa67af822842e9fc7a32d93b1b57d1a05
Jennie Maes <jennie.maes@ruralsourcing.com>
2022-12-06 20:19:45 -0700

    Add a simple functional solution for Day 6's problem

---
+++ Day-06-TuningTrouble/Program.fs
@@ -0,0 +1,23 @@
+﻿open System.IO
+
+let findMarker (windowSize: int) (text: string) : int option =
+    Seq.cast text
+    |> Seq.windowed windowSize
+    |> Seq.indexed
+    |> Seq.map (fun (idx: int, window: char []) -> (idx, Set.count <| Set.ofArray window))
+    |> Seq.tryFind (snd >> ((=) windowSize))
+    |> Option.map (fst >> ((+) windowSize))
+
+[<EntryPoint>]
+let main (args: string []) =
+    let input: string = File.ReadAllText "./input"
+
+    printfn "--- Part One ---"
+    printfn "How many characters need to be processed before the first start-of-packet marker is detected?"
+    printfn "Answer: %A\n" (findMarker 4 input)
+
+    printfn "--- Part Two ---"
+    printfn "How many characters need to be processed before the first start-of-message marker is detected?"
+    printfn "Answer: %A\n" (findMarker 14 input)
+
+    0
