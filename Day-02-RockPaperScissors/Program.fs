91b0f078ee0d059dca8b1adde88e7c2df193140d
Jennie Maes <jennie.maes@ruralsourcing.com>
2022-12-06 20:18:46 -0700

    Tidy up RockPaperScissors a little bit

--- Day-02-RockPaperScissors/Program.fs
+++ Day-02-RockPaperScissors/Program.fs
@@ -13,55 +13,59 @@ type LoseDrawWin =
     | Draw = 3
     | Win = 6
 
-let rockPaperScissorsScore (abc: string) (xyz: string) : int =
-    let myMove: int =
-        match xyz with
-        | "X" -> int RockPaperScissors.Rock
-        | "Y" -> int RockPaperScissors.Paper
-        | "Z" -> int RockPaperScissors.Scissors
+module RockPaperScissors =
+    let score (abc: string) (xyz: string) : int =
+        let myMove: int =
+            match xyz with
+            | "X" -> int RockPaperScissors.Rock
+            | "Y" -> int RockPaperScissors.Paper
+            | "Z" -> int RockPaperScissors.Scissors
 
-    let scoreKey: LoseDrawWin [] = LoseDrawWin.GetValues()
+        let scoreKey: LoseDrawWin [] = LoseDrawWin.GetValues()
 
-    let myScore: int =
-        match abc with
-        | "A" -> int scoreKey[myMove % 3]
-        | "B" -> int scoreKey[myMove - 1]
-        | "C" -> int scoreKey[(myMove + 1) % 3]
+        let myScore: int =
+            match abc with
+            | "A" -> int scoreKey[myMove % 3]
+            | "B" -> int scoreKey[myMove - 1]
+            | "C" -> int scoreKey[(myMove + 1) % 3]
 
-    myMove + myScore
+        myMove + myScore
 
-let loseDrawWinScore (abc: string) (xyz: string) : int =
-    let myScore: int =
-        match xyz with
-        | "X" -> int LoseDrawWin.Lose
-        | "Y" -> int LoseDrawWin.Draw
-        | "Z" -> int LoseDrawWin.Win
+module LoseDrawWin =
+    let score (abc: string) (xyz: string) : int =
+        let myScore: int =
+            match xyz with
+            | "X" -> int LoseDrawWin.Lose
+            | "Y" -> int LoseDrawWin.Draw
+            | "Z" -> int LoseDrawWin.Win
 
-    let moveKey: RockPaperScissors [] = RockPaperScissors.GetValues()
+        let moveKey: RockPaperScissors [] = RockPaperScissors.GetValues()
 
-    let myMove: int =
-        match abc with
-        | "A" -> int moveKey[(myScore / 3 + 2) % 3]
-        | "B" -> int moveKey[myScore / 3]
-        | "C" -> int moveKey[(myScore / 3 + 1) % 3]
+        let myMove: int =
+            match abc with
+            | "A" -> int moveKey[(myScore / 3 + 2) % 3]
+            | "B" -> int moveKey[myScore / 3]
+            | "C" -> int moveKey[(myScore / 3 + 1) % 3]
 
-    myMove + myScore
+        myMove + myScore
 
 [<EntryPoint>]
 let main (args: string []) =
     let checkFormat: Regex = Regex @"^[ABC]\s+[XYZ]$"
 
+    let splitTuple (line: string) : string * string =
+        let pair: string [] = line.Split [||] in (pair[0], pair[1])
+
     let input: seq<string * string> =
         Seq.cast (File.ReadAllLines(@"./input"))
         |> Seq.filter checkFormat.IsMatch
-        |> Seq.map (fun (line: string) -> line.Split null)
-        |> Seq.map (fun [| abc: string; xyz: string |] -> (abc, xyz))
+        |> Seq.map splitTuple
 
     printfn "--- Part One ---"
     printfn "What would your total score be if everything goes exactly according to your strategy guide?"
 
     input
-    |> Seq.map (fun (moves: string * string) -> moves ||> rockPaperScissorsScore)
+    |> Seq.map ((<||) RockPaperScissors.score)
     |> Seq.sum
     |> printfn "Answer: %d\n"
 
@@ -70,7 +74,7 @@ let main (args: string []) =
     printfn "exactly according to your strategy guide?"
 
     input
-    |> Seq.map (fun (moves: string * string) -> moves ||> loseDrawWinScore)
+    |> Seq.map ((<||) LoseDrawWin.score)
     |> Seq.sum
     |> printfn "Answer: %d\n"
 
